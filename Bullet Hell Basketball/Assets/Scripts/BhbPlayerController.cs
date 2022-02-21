ae85d441b5b4e3b75e6c3288fa332d029fdb3053
Nick Baker <nickmbaker1@gmail.com>
2022-02-21 13:30:44 -0500

    controller support, own goal fix, etc.

--- Bullet Hell Basketball/Assets/Scripts/BhbPlayerController.cs
+++ Bullet Hell Basketball/Assets/Scripts/BhbPlayerController.cs
@@ -18,8 +18,10 @@ public class BhbPlayerController : NeonHeightsCharacterController
 {
     private KeyCode[] player1Controls = { KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.Space, KeyCode.B, KeyCode.N, KeyCode.Escape };
     private KeyCode[] player2Controls = { KeyCode.P, KeyCode.Semicolon, KeyCode.L, KeyCode.Quote, KeyCode.RightControl, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.Escape };
-    private string[] player1GamepadControls = { "","" , "", ""};
+    private string[] gamepadControls = { "Vertical", "DVertical", "Horizontal", "DHorizontal", "A", "B", "X", "Y", "Start" };
 
+
+    public int controllerNumber = -1;
     public int playerNumber;
     public float pickupRadius;
     public Vector3 playerHandPos;
@@ -27,14 +29,17 @@ public class BhbPlayerController : NeonHeightsCharacterController
     private GameObject ball;
     private Ball ballScript;
     private BhbBallPhysics ballPhysics;
-    private float timer;
+    private float autoCatchCooldownTimer;
+
+    public float autoCatchCooldownTimerMax = 1;
 
     public Material player2Sprite;
 
-    public void Init(int playerNumber){
+    public void Init(int playerNumber)
+    {
         this.playerNumber = playerNumber;
 
-        if(playerNumber == 1)
+        if (playerNumber == 1)
         {
             gameObject.GetComponent<MeshRenderer>().material = player2Sprite;
         }
@@ -82,57 +87,24 @@ public class BhbPlayerController : NeonHeightsCharacterController
 
         if (GetControlDown(Control.Action))
         {
-            //If outside the range to pickup the ball, apply cooldown.
-            //(Implement dive/tackle/steal in future.)
-            if (Vector2.Distance(ball.transform.position, gameObject.transform.position) > pickupRadius)
-            {
-                //apply action cooldown
-                //give visual feedback that it's on cooldown (grey out jerma)
-                return;
-            }
-
             //if holding the ball...
-            if (ball.transform.parent)
+            if (ball.transform.parent == transform)
             {
+                autoCatchCooldownTimer = 0;
                 ballScript.ShootBall(playerNumber);
             }
-            else
+            else if (Vector2.Distance(ball.transform.position, transform.position) < 5.0f && autoCatchCooldownTimer > autoCatchCooldownTimerMax && ball.transform.parent != transform && ball.transform.parent != null)
             {
-                ballPhysics.simulatePhysics = false;
-
-                //reverses the x-coord for second player.
-                if(playerNumber == 1)
-                {
-                    ball.transform.position = (gameObject.transform.position + new Vector3(playerHandPos.x * -1, playerHandPos.y, playerHandPos.z));
-                }
-                else
-                {
-                    ball.transform.position = (gameObject.transform.position + playerHandPos);
-                }
-                ball.transform.parent = gameObject.transform;
+                GrabBall();
             }
         }
-
-        //pickup on radius < 5.0f
-        if (Vector2.Distance(ball.transform.position, transform.position) < 5.0f && timer > 1.5f)
+        else if (Vector2.Distance(ball.transform.position, transform.position) < 5.0f && autoCatchCooldownTimer > autoCatchCooldownTimerMax && ball.transform.parent == null)
         {
-            timer = 0;
-            ballPhysics.simulatePhysics = false;
-
-            //reverses the x-coord for second player.
-            if (playerNumber == 1)
-            {
-                ball.transform.position = (gameObject.transform.position + new Vector3(playerHandPos.x * -1, playerHandPos.y, playerHandPos.z));
-            }
-            else
-            {
-                ball.transform.position = (gameObject.transform.position + playerHandPos);
-            }
-            ball.transform.parent = gameObject.transform;
+            GrabBall();
         }
-        if (timer < 2)
+        if (autoCatchCooldownTimer < autoCatchCooldownTimerMax + .5f)
         {
-            timer += Time.deltaTime;
+            autoCatchCooldownTimer += Time.deltaTime;
         }
 
         stoppedJumping = GetControlUp(Control.Jump);
@@ -140,8 +112,30 @@ public class BhbPlayerController : NeonHeightsCharacterController
         UpdateAugust();
     }
 
+    public void GrabBall()
+    {
+        autoCatchCooldownTimer = 0;
+        ballPhysics.simulatePhysics = false;
+
+        //reverses the x-coord for second player.
+        if (playerNumber == 1)
+        {
+            ball.transform.position = (gameObject.transform.position + new Vector3(playerHandPos.x * -1, playerHandPos.y, playerHandPos.z));
+        }
+        else
+        {
+            ball.transform.position = (gameObject.transform.position + playerHandPos);
+        }
+        ball.transform.parent = gameObject.transform;
+    }
+
     bool GetControlHeld(Control action)
     {
+
+        if (GetGamepadControlHeld(action))
+        {
+            return true;
+        }
 
         //if(Input.GetButton())
         if (playerNumber == 0)
@@ -171,6 +165,11 @@ public class BhbPlayerController : NeonHeightsCharacterController
 
     bool GetControlDown(Control action)
     {
+        if (GetGamepadControlDown(action))
+        {
+            return true;
+        }
+
         if (playerNumber == 0)
         {
             if (action == Control.Jump)
@@ -198,6 +197,11 @@ public class BhbPlayerController : NeonHeightsCharacterController
 
     bool GetControlUp(Control action)
     {
+        if (GetGamepadControlUp(action))
+        {
+            return true;
+        }
+
         if (playerNumber == 0)
         {
             if (action == Control.Jump)
@@ -220,6 +224,145 @@ public class BhbPlayerController : NeonHeightsCharacterController
                 return Input.GetKeyUp(player2Controls[((int)action)]);
             }
         }
+        return false;
+    }
+
+
+    // Up,
+    // Down,
+    // Left,
+    // Right,
+    // Jump,
+    // Jump2,
+    // Action,
+    // Pause
+    bool GetGamepadControlHeld(Control action)
+    {
+        if (controllerNumber == -1)
+            return false;
+
+
+        string gamepadIdentifier = "J" + controllerNumber;
+
+        if (action == Control.Up)
+        {
+            return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) > 0;
+        }
+        else if (action == Control.Down)
+        {
+            return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) < 0;
+        }
+
+        if (action == Control.Right)
+        {
+            return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) > 0;
+        }
+        else if (action == Control.Left)
+        {
+            return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) < 0;
+        }
+
+
+        if (action == Control.Jump || action == Control.Jump2)
+        {
+            return Input.GetButton(gamepadIdentifier + gamepadControls[4]) || Input.GetButton(gamepadIdentifier + gamepadControls[5]);
+        }
+        else if (action == Control.Action)
+        {
+            return Input.GetButton(gamepadIdentifier + gamepadControls[6]) || Input.GetButton(gamepadIdentifier + gamepadControls[7]);
+        }
+        else if (action == Control.Pause)
+        {
+            return Input.GetButton(gamepadIdentifier + gamepadControls[8]);
+        }
+
+        return false;
+    }
+
+
+    bool GetGamepadControlDown(Control action)
+    {
+        if (controllerNumber == -1)
+            return false;
+
+
+        string gamepadIdentifier = "J" + controllerNumber;
+
+        // if (action == Control.Up)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) > 0;
+        // }
+        // else if (action == Control.Down)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) < 0;
+        // }
+
+        // if (action == Control.Right)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) > 0;
+        // }
+        // else if (action == Control.Left)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) < 0;
+        // }
+
+
+        if (action == Control.Jump || action == Control.Jump2)
+        {
+            return Input.GetButtonDown(gamepadIdentifier + gamepadControls[4]) || Input.GetButtonDown(gamepadIdentifier + gamepadControls[5]);
+        }
+        else if (action == Control.Action)
+        {
+            return Input.GetButtonDown(gamepadIdentifier + gamepadControls[6]) || Input.GetButtonDown(gamepadIdentifier + gamepadControls[7]);
+        }
+        else if (action == Control.Pause)
+        {
+            return Input.GetButtonDown(gamepadIdentifier + gamepadControls[8]);
+        }
+
+        return false;
+    }
+
+    bool GetGamepadControlUp(Control action)
+    {
+        if (controllerNumber == -1)
+            return false;
+
+
+        string gamepadIdentifier = "J" + controllerNumber;
+
+        // if (action == Control.Up)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) > 0;
+        // }
+        // else if (action == Control.Down)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[0]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[1]) < 0;
+        // }
+
+        // if (action == Control.Right)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) > .3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) > 0;
+        // }
+        // else if (action == Control.Left)
+        // {
+        //     return Input.GetAxis(gamepadIdentifier + gamepadControls[2]) < -.3 || Input.GetAxis(gamepadIdentifier + gamepadControls[3]) < 0;
+        // }
+
+
+        if (action == Control.Jump || action == Control.Jump2)
+        {
+            return Input.GetButtonUp(gamepadIdentifier + gamepadControls[4]) || Input.GetButtonUp(gamepadIdentifier + gamepadControls[5]);
+        }
+        else if (action == Control.Action)
+        {
+            return Input.GetButtonUp(gamepadIdentifier + gamepadControls[6]) || Input.GetButtonUp(gamepadIdentifier + gamepadControls[7]);
+        }
+        else if (action == Control.Pause)
+        {
+            return Input.GetButtonUp(gamepadIdentifier + gamepadControls[8]);
+        }
+
         return false;
     }
 }
