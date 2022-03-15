96007f8120030577ab9a4bef0b6e2f90e2fea5d2
Brian Kiss <bjk4726@rit.edu>
2022-03-15 09:41:05 -0400

    Switched over audio to manager

--- Bullet Hell Basketball/Assets/Scripts/AudioManager.cs
+++ Bullet Hell Basketball/Assets/Scripts/AudioManager.cs
@@ -32,6 +32,11 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        Play("Music");
+    }
+
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
