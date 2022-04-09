6275dc187a082cd70bdf8fe39e4363d1df21efe6
Brian Kiss <bjk4726@rit.edu>
2022-04-08 20:14:08 -0400

    Added camera shake on 'esc'

--- Bullet Hell Basketball/Assets/Scripts/BulletExplosion.cs
+++ Bullet Hell Basketball/Assets/Scripts/BulletExplosion.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// For bullets that explode. Handles playing explosion particles and camera shake.
+/// </summary>
 public class BulletExplosion : MonoBehaviour
 {
     private ParticleSystem ps;
+    private CameraShake cameraShake;
 
     // Start is called before the first frame update
     void Start()
     {
         ps = GetComponent<ParticleSystem>();
         ps.Stop();
+        cameraShake = FindObjectOfType<Camera>().GetComponent<CameraShake>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -19,6 +25,7 @@ public class BulletExplosion : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ps.Play();
+            StartCoroutine(cameraShake.Shake(.2f, .5f));
         }
         //if (!ps.isEmitting) //if no longer emitting, destroy prefab.
         //{
