b515ae0c6a152f4d1c64b8b939f35a52515debe5
Nick Baker <nickmbaker1@gmail.com>
2022-04-13 04:04:37 -0400

    Organized the workflow behind creating buttons (only on the first menu screen though). Added properly scaling button borders that dont stretch the rounded corners. Refer to discord message for more info about all this stuff.

--- Bullet Hell Basketball/Assets/Scripts/HologramButton.cs
+++ Bullet Hell Basketball/Assets/Scripts/HologramButton.cs
@@ -5,40 +5,53 @@ using UnityEngine.EventSystems;// Required when using Event data.
 
 public class HologramButton : MonoBehaviour, ISelectHandler, IDeselectHandler// required interface when using the OnSelect method.
 {
-	//Do this when the selectable UI object is selected.
-	
-	public void OnSelect(BaseEventData eventData)
-	{
-		transform.GetChild(0).gameObject.SetActive(true);
-		transform.GetChild(1).gameObject.SetActive(false);
+    //Do this when the selectable UI object is selected.
 
-		if (transform.GetChild(4).gameObject != null)
-		{
-			transform.GetChild(3).gameObject.SetActive(true);
-			transform.GetChild(4).gameObject.SetActive(false);
-		}
-	}
+    public void OnSelect(BaseEventData eventData)
+    {
+        SelectVisual();
+        // transform.GetChild(0).gameObject.SetActive(true);
+        // transform.GetChild(1).gameObject.SetActive(false);
 
-	public void OnDeselect(BaseEventData eventData)
-	{
-		transform.GetChild(1).gameObject.SetActive(true);
-		transform.GetChild(0).gameObject.SetActive(false);
+        // if (transform.GetChild(4).gameObject != null)
+        // {
+        // 	transform.GetChild(3).gameObject.SetActive(true);
+        // 	transform.GetChild(4).gameObject.SetActive(false);
+        // }
+    }
 
-		if (transform.GetChild(4).gameObject != null)
-		{
-			transform.GetChild(4).gameObject.SetActive(true);
-			transform.GetChild(3).gameObject.SetActive(false);
-		}
-	}
-	
+    public void OnDeselect(BaseEventData eventData)
+    {
+        DeselectVisual();
 
-	public void OnMouseOver()
-	{
-		Debug.Log(" waatrasrsdtsard selected");
-	}
+        // transform.GetChild(1).gameObject.SetActive(true);
+        // transform.GetChild(0).gameObject.SetActive(false);
 
-	public void OnMouseExit()
-	{
-		Debug.Log(this.gameObject.name + "rats art art at selected");
-	}
+        // if (transform.GetChild(4).gameObject != null)
+        // {
+        // 	transform.GetChild(4).gameObject.SetActive(true);
+        // 	transform.GetChild(3).gameObject.SetActive(false);
+        // }
+    }
+
+    private void SelectVisual()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void DeselectVisual()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        SelectVisual();
+    }
+
+    public void OnMouseExit()
+    {
+        if (EventSystem.current.currentSelectedGameObject != gameObject)
+            DeselectVisual();
+    }
 }
